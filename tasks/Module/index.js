aa5a10f3a277a0fea209ae3ff10bfd2b812efb03
Ihar Berasneu <igor.simatic@gmail.com>
2021-07-19 14:24:42 +0300

    refactor: added intends

--- tasks/Module/index.js
+++ tasks/Module/index.js
@@ -1,26 +1,23 @@
 const readline = require('readline');
+const weather = require('weather-js');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question('Please, enter your city on English? ', (answer) => {
-  // TODO: Log the answer in a database  
-  const weather = require('weather-js'); 
-  
-  weather.find({search: answer, degreeType: 'C'}, function(err, result) {
-    if(err) console.log(err);        
-    const info = `
-    Location: ${result[0].location.name}
-    Sky: ${result[0].current.skytext} 
-    Temperature: ${result[0].current.temperature}
-    Wind: ${result[0].current.winddisplay} 
-     `
-    console.log(info)
-    
+rl.question('Please, enter your city in English? ', (answer) => {
+  weather.find({ search: answer, degreeType: 'C' }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`
+        Location: ${result[0].location.name}
+        Sky: ${result[0].current.skytext} 
+        Temperature: ${result[0].current.temperature}
+        Wind: ${result[0].current.winddisplay}`
+      );
+    }
   });
-
   rl.close();
-    
 });
