1e77ae6f3efe3ad35efbcdcc35316def62662c13
Ihar Berasneu <igor.simatic@gmail.com>
2021-08-28 00:01:28 +0300

    feat:add Posts and styles

--- tasks/PixelPerfectApp/gulpfile.js
+++ tasks/PixelPerfectApp/gulpfile.js
@@ -6,6 +6,9 @@ const htmlmin = require('gulp-htmlmin');
 const del = require('del');
 const newer = require('gulp-newer');
 const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 
 
@@ -46,8 +49,29 @@ function styles() {
   return gulp.src(paths.styles.src)
     .pipe(sourcemaps.init())
     .pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
-    .pipe(sourcemaps.write('./'))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(cleanCSS({
+      level: 2
+    }))
+    .pipe(rename({
+      basename: 'style',
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(size({
+      showFiles: true
+    }))
     .pipe(gulp.dest(paths.styles.dest));
+
+  /* 
+  .pipe(browsersync.stream())
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.styles.dest)); */
 }
 
 function img() {
