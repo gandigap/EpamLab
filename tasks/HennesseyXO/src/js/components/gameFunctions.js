d84badb8058fd056903ca465e4265b2b237885af
Ihar Berasneu <igor.simatic@gmail.com>
2021-09-13 16:19:18 +0300

    feat:add function for change field cells

---
+++ tasks/HennesseyXO/src/js/components/gameFunctions.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-expressions */
+import create from '../create';
+
+export function createField() {
+  const body = document.querySelector('body');
+  create('div', 'field', '', body);
+  const field = document.querySelector('.field');
+  for (let index = 0; index < 9; index += 1) {
+    create('div', 'field__cell', '', field, ['id', index + 1]);
+  }
+}
+
+
+
+
+
+
+
+
