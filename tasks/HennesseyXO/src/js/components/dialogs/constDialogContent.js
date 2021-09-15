9054c74203ba1c9008d8064e19eaf84a567dc1d4
Ihar Berasneu <igor.simatic@gmail.com>
2021-09-15 23:36:00 +0300

    refactor:add styles and change function for display result game

--- tasks/HennesseyXO/src/js/components/dialogs/constDialogContent.js
+++ tasks/HennesseyXO/src/js/components/dialogs/constDialogContent.js
@@ -1,17 +1,8 @@
 const dialogContent = {
-  startContent: `
-  <h3 class='dialogContainer__title'>HennesseyXO</h3>
-  <p class='dialogContainer__subtitle'>Please enter player names</p>
-  <div class='dialogContainer__inputContainer'>
-    <label class='dialogContainer__inputContainer__label' for="player1name">Player1 :</label>
-    <input class='dialogContainer__inputContainer__input' type="text" id="player1name" placeholder='name'>
-  </div>
-  <div class='dialogContainer__inputContainer'>
-    <label class='dialogContainer__inputContainer__label' for="player2name">Player2 :</label>
-    <input class='dialogContainer__inputContainer__input' type="text" id="player2name" placeholder='name'>
-  </div>
-  <button class='dialogContainer__button' id='button-startGame'>Start</button>`,
-  winContent: "<h3 class='dialogContainer__title'>Congratulations</h3><button class='dialogContainer__button' id='button-startGame'>Start</button>",
-  evenContent: "<h3 class='dialogContainer__title'>Even</h3><button class='dialogContainer__button' id='button-startGame'>Start</button>",
+  winContent: `
+  <h3 class='dialogContainer__title'>Win</h3>
+  <button class='dialogContainer__button' id='button-startGame'>Restart</button>`,
+  evenContent: `<h3 class='dialogContainer__title'>Even</h3>
+  <button class='dialogContainer__button' id='button-startGame'>Restart</button>`,
 };
 export default dialogContent;
--- tasks/HennesseyXO/src/js/components/dialogs/dialogActions.js
+++ tasks/HennesseyXO/src/js/components/dialogs/dialogActions.js
@@ -1,32 +1,25 @@
 import create from '../constructor/create';
-import gameState from '../state/state';
-import { startGame } from '../actions/gameActions';
 import dialogContent from './constDialogContent';
+import gameState from '../state/state';
 
 export function changeStateDialog(state) {
   const dialogContainer = document.querySelector('.dialogContainer');
   // eslint-disable-next-line no-unused-expressions
   state === 'open' ? dialogContainer.showModal() : dialogContainer.close();
 }
-function setPlayerNames() {
-  const { playerNames } = gameState;
-  const inputsName = document.querySelectorAll('.dialogContainer__inputContainer__input');
-  playerNames.firstPlayerName = inputsName[0].value;
-  playerNames.secondPlayerName = inputsName[1].value;
-  changeStateDialog('close');
-  startGame();
+
+function setContentDialog(dialogContainer, status) {
+  dialogContainer.innerHTML = status === 'win'
+    ? `<p class="dialogContainer__winner">
+        ${gameState.currentMark === 'x' ? gameState.players.firstName : gameState.players.secondName} 
+       </p> ${dialogContent.winContent}`
+    : dialogContent.evenContent;
 }
 
-function setStartContentDialog(dialogContainer) {
-  dialogContainer.innerHTML = dialogContent.startContent;
-  const buttonStart = document.getElementById('button-startGame');
-  buttonStart.addEventListener('click', setPlayerNames, false);
-}
-
-export function createDialog() {
+export function createDialog(status) {
   const body = document.querySelector('body');
   create('dialog', 'dialogContainer', '', body);
   const dialog = document.querySelector('.dialogContainer');
-  setStartContentDialog(dialog)
+  setContentDialog(dialog, status);
   changeStateDialog('open');
 }
